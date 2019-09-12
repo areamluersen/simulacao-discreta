094443237755a0e609ff3a316d2d01fec802be85
Gustavo Krieger <gustavopcassol@gmail.com>
2019-09-11 22:57:11 -0300

    adiciona entrada de usuario

---
+++ src/entrada.py
@@ -0,0 +1,126 @@
+import sys
+
+from src.numeros_randomicos import metodoJson
+
+TEXTO_DE_ERRO_NA_ENTRADA = 'Erro na entrada.'
+
+
+def selecaoMetodoDoUsuario():
+    entradaUsuario = fazEntradaSelecaoMetodoDoUsuario()
+    validaSelecaoMetodoDoUsuario(entradaUsuario)
+
+    return entradaUsuario
+
+
+def fazEntradaSelecaoMetodoDoUsuario():
+    print('Digite o metodo a ser executado:')
+    print(f'1) {metodoJson[1]}')
+    print(f'2) {metodoJson[2]}')
+    print(f'3) {metodoJson[3]}')
+    print(f'4) {metodoJson[4]}')
+    entradaUsuario = input('entrada: ')
+
+    return entradaUsuario
+
+
+def validaSelecaoMetodoDoUsuario(entradaUsuario):
+    verificaTipoSelecaoMetodoDoUsuario(entradaUsuario)
+    verificaValorSelecaoMetodoDoUsuario(entradaUsuario)
+
+
+def verificaTipoSelecaoMetodoDoUsuario(entradaUsuario):
+    try:
+        int(entradaUsuario)
+    except ValueError:
+        tratarErroDeEntrada()
+
+
+def tratarErroDeEntrada():
+    print(TEXTO_DE_ERRO_NA_ENTRADA)
+    sys.exit()
+
+
+def verificaValorSelecaoMetodoDoUsuario(entradaUsuario):
+    numeroDeMetodos = len(metodoJson)
+    entradaUsuario = int(entradaUsuario)
+
+    if entradaUsuario < 1 or entradaUsuario > numeroDeMetodos:
+        tratarErroDeEntrada()
+
+
+def selecaoDeValoresDoUsuario():
+    entradasUsuario = fazEntradaSelecaoDeValoresDoUsuarioComValidacao()
+
+    return entradasUsuario
+
+
+def selecaoDeQuantidade():
+    entradaUsuario = fazEntradaDeQuantidade()
+    validaSelecaoDeQuantidade(entradaUsuario)
+
+    return entradaUsuario
+
+
+def validaSelecaoDeQuantidade(entradaUsuario):
+    try:
+        int(entradaUsuario)
+    except ValueError:
+        tratarErroDeEntrada()
+
+
+def fazEntradaDeQuantidade():
+    return input('Digite a quantidade: ')
+
+
+def fazEntradaSelecaoDeValoresDoUsuarioComValidacao():
+    entradasUsuario = {}
+
+    print('Digite apenas os valores necessarios:')
+
+    entrada = input('Limite inferior: ')
+    validaSelecaoDeValorDoUsuario(entrada)
+    entradasUsuario['valorMenor'] = entrada
+
+    entrada = input('Limite superior: ')
+    validaSelecaoDeValorDoUsuario(entrada)
+    entradasUsuario['valorMaior'] = entrada
+
+    entrada = input('Moda: ')
+    validaSelecaoDeValorDoUsuario(entrada)
+    entradasUsuario['moda'] = entrada
+
+    entrada = input('Media: ')
+    validaSelecaoDeValorDoUsuario(entrada)
+    entradasUsuario['media'] = entrada
+
+    entrada = input('Variancia: ')
+    validaSelecaoDeValorDoUsuario(entrada)
+    entradasUsuario['variancia'] = entrada
+
+    return entradasUsuario
+
+
+def validaSelecaoDeValorDoUsuario(entradaUsuario):
+    verificaTipoSelecaoDeValorDoUsuario(entradaUsuario)
+
+
+def verificaTipoSelecaoDeValorDoUsuario(entradaUsuario):
+    if entradaUsuario == '':
+        return
+
+    try:
+        float(entradaUsuario)
+    except ValueError:
+        tratarErroDeEntrada()
+
+
+def converteValoresDeEntrada(entradasUsuario):
+    return dict([k, float(v)] for k, v in entradasUsuario.items() if v != '')
+
+
+def converteSelecaoMetodoDoUsuario(entradaUsuario):
+    return int(entradaUsuario)
+
+
+def converteSelecaoDeQuantidade(entradaUsuario):
+    return int(entradaUsuario)
--- src/numeros_randomicos.py
+++ src/numeros_randomicos.py
@@ -10,11 +10,11 @@ metodoJson = {
 }
 
 # x = a + (b-a)*R   R = Random number - a={valor menor} b={valor maior}
-def gerarRandomicoUniforme(valorMenor=0, valorMaior=1):
+def gerarRandomicoUniforme(valorMenor, valorMaior):
     return valorMenor + (valorMaior - valorMenor)*random.random()
 
 # Slide professor.
-def gerarRandomicoTriangular(valorMenor=0, valorMaior=1, moda= 0.5):
+def gerarRandomicoTriangular(valorMenor, valorMaior, moda):
     U = random.random()
     if (U < (moda-valorMenor)/(valorMaior-valorMenor)):
         return valorMenor + (U*(moda-valorMenor)*(valorMaior-valorMenor)) ** 0.5
@@ -22,7 +22,7 @@ def gerarRandomicoTriangular(valorMenor=0, valorMaior=1, moda= 0.5):
         return valorMaior - ((1-U)*(valorMaior-moda)*(valorMaior-valorMenor)) ** 0.5
 
 
-def gerarRandomicoExponencial(limiteInferior=0, media=5):
+def gerarRandomicoExponencial(limiteInferior, media):
     # http://mpsantos.com.br/simul.pdf - Página 66 e 67
     U = random.random()
     alpha = 1 / (media - limiteInferior)
@@ -30,7 +30,7 @@ def gerarRandomicoExponencial(limiteInferior=0, media=5):
     return x
 
 
-def gerarRandomicoNatural(media=5, variancia=1):
+def gerarRandomicoNatural(media, variancia):
     U1 = random.random()
     U2 = random.random()
     Z = math.sqrt(-2 * math.log(U1)) * math.sin(2 * math.pi * U2)
@@ -38,7 +38,7 @@ def gerarRandomicoNatural(media=5, variancia=1):
     return X
 
 
-def gerarNumerosRandomicos(qtde=10, metodo=1, valorMenor=0, valorMaior=1, moda=0.5, media=5, variancia=1):
+def gerarNumerosRandomicos(metodo, qtde, valorMenor=None, valorMaior=None, moda=None, media=None, variancia=None):
     numerosAleatorios = []
     if metodoJson[metodo] == 'uniforme':
         for _ in range(qtde):
@@ -57,13 +57,3 @@ def gerarNumerosRandomicos(qtde=10, metodo=1, valorMenor=0, valorMaior=1, moda=0
             numerosAleatorios.append(
                 gerarRandomicoNatural(media, variancia))
     return numerosAleatorios
-
-numerosAleatorios = gerarNumerosRandomicos(50, 2, 1 , 100, 8)
-acc = 0
-for i in range(len(numerosAleatorios)):
-    acc += numerosAleatorios[i]
-    print('numero',i+1, ':', numerosAleatorios[i])
-with open('output.dst', 'w') as file:
-    for item in numerosAleatorios:
-        file.write(str(item) + '\n')
-print ('Valor medio : ', acc/len(numerosAleatorios))
