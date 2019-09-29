bc9db91ac63d1353c988a66b32c643d4166b96b8
Aream <areamluersern@gmail.com>
2019-09-29 19:38:57 -0300

    comentarios lavacar

---
+++ src/lavacar/main.py
@@ -0,0 +1,17 @@
+
+
+# 1- Criar array de chegadas (aleatório ou deterministico) com Tempo total definido pelo usuário em minutos.
+# 2- Criar Array de tempos de atendimento (aleatório ou deterministico). 
+# 3- Definir se tamanho das filas terão limites (excluir unidades que chagarem e estrapolarem tamanho da fila).
+# 4- while chagadas.langth > i i++ - loop
+# 5- Dentro do while i vale como minutos - 
+# 6- Ir adicionando as chegadas e realizando os atendimentos, armazenando valores das variáveis de interesse.
+
+
+# def tempo_chegadas(aleatorio=True, tempo_total_min):
+
+# def tempo_atendimentos(aleatorio=True, tempo_total_min):
+
+a = [{'a': 'olá', 'b': 'não'}, {}]
+
+print(a[0]['a'])
