088b8c6cb2f8ecbf0ef936407df6b885b0f111f7
Gustavo Krieger <gustavopcassol@gmail.com>
2019-09-29 19:53:26 -0300

    começo do main

--- src/lavacar/main.py
+++ src/lavacar/main.py
@@ -1,5 +1,3 @@
-
-
 # 1- Criar array de chegadas (aleatório ou deterministico) com Tempo total definido pelo usuário em minutos.
 # 2- Criar Array de tempos de atendimento (aleatório ou deterministico). 
 # 3- Definir se tamanho das filas terão limites (excluir unidades que chagarem e estrapolarem tamanho da fila).
@@ -7,8 +5,19 @@
 # 5- Dentro do while i vale como minutos - 
 # 6- Ir adicionando as chegadas e realizando os atendimentos, armazenando valores das variáveis de interesse.
 
-# Variáveis : (status_servidor(liberado, em_uso)) fila. cliente 
+# Variáveis : (status_servidor(liberado, em_uso)) fila. cliente
 
-cliente = [{'minutoChegada': 5, 'tempo_': 'não'}, {}]
 
-print(a[0]['a'])
+def main():
+    chegadas = [1, 3, 5]
+    tempo_total = 5
+    antendimentos = []
+
+    cliente = [{'minutoChegada': 5, 'tempo_': 'não'}, {}]
+
+    for _ in range(tempo_total):
+        None
+
+
+if __name__ == '__main__':
+    main()
