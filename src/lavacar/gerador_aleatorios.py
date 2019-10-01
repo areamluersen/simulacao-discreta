cb771af3031931af39e5e9038e36616a116413c5
Aream <areamluersern@gmail.com>
2019-10-01 19:00:10 -0300

    adicionado logica inicial simulacao

--- src/lavacar/gerador_aleatorios.py
+++ src/lavacar/gerador_aleatorios.py
@@ -8,21 +8,17 @@ def gerar_tempos_de_distribuicao(aleatorio=True, tempo_total_min=540, distribuic
         for k, v in distribuicao.items():
 	        if (aleatorio < k):
                   tempo_acumulado += v
-                  tempo_chegadas.append(v)
+                  tempo_chegadas.append({'minuto_tempo': tempo_acumulado, 'intervalo': v})
                   break
     return tempo_chegadas
 
 
-distribuicao_chegadas = { 25: 12, 50: 13, 77: 14, 100: 15 }
-distribuicao_atendimentos = { 30: 11, 70: 14, 85: 15, 100: 16 }
-tempo_chegadas = gerar_tempos_de_distribuicao(True, 800, distribuicao_chegadas)
-tempo_atendimentos = gerar_tempos_de_distribuicao(True, 800, distribuicao_atendimentos)
+# distribuicao_chegadas = { 25: 12, 50: 13, 77: 14, 100: 15 }
+# distribuicao_atendimentos = { 30: 11, 70: 14, 85: 15, 100: 16 }
+# tempo_chegadas = gerar_tempos_de_distribuicao(True, 800, distribuicao_chegadas)
+# tempo_atendimentos = gerar_tempos_de_distribuicao(True, 800, distribuicao_atendimentos)
 
-relogio_chegada = 0
-relogio_atendimento = 0
-for i in range(min(len(tempo_chegadas), len(tempo_atendimentos))):
-    relogio_chegada += tempo_chegadas[i]
-    relogio_atendimento += tempo_atendimentos[i]
-    print('Chegada no min: ', relogio_chegada)
-    print('atendimento no min: ', relogio_atendimento)
+# for i in range(min(len(tempo_chegadas), len(tempo_atendimentos))):
+#     print('Chegada no min: ', tempo_chegadas[i]['minuto_tempo'])
+#     print('atendimento no min: ', tempo_atendimentos[i]['minuto_tempo'])
 
--- src/lavacar/main.py
+++ src/lavacar/main.py
@@ -6,17 +6,56 @@
 # 6- Ir adicionando as chegadas e realizando os atendimentos, armazenando valores das variáveis de interesse.
 
 # Variáveis : (status_servidor(liberado, em_uso)) fila. cliente
+import gerador_aleatorios
 
+#  cliente = [{'minutoChegada': 5, 'tempo_': 'não'}, {}]
 
 def main():
-    chegadas = [1, 3, 5]
-    tempo_total = 5
-    antendimentos = []
+    # Variáveis que devem ser lidas do usuário
+    tempo_simulacao_min = 540 #Equivalente a 8 horas
+    distribuicao_chegadas = { 25: 12, 50: 13, 77: 14, 100: 15 }
+    distribuicao_atendimentos = { 30: 11, 70: 14, 85: 15, 100: 16 }
+    tempo_chegadas = gerador_aleatorios.gerar_tempos_de_distribuicao(True, tempo_simulacao_min, distribuicao_chegadas)
+    tempo_atendimentos = gerador_aleatorios.gerar_tempos_de_distribuicao(True, tempo_simulacao_min, distribuicao_atendimentos)
+    # Variáveis que devem ser lidas do usuário
 
-    cliente = [{'minutoChegada': 5, 'tempo_': 'não'}, {}]
+    #variáveis discretas
+    servidor_ocupado = False
+    atendimentos_realizados_cont = 0
+    atendimentos_realizados_inf = []
+    fila = []
+    posicao_lista_chegada = 0
+    servidor_ficara_livre_no_min=0
+    #variáveis discretas
 
-    for _ in range(tempo_total):
-        None
+    for i in range(tempo_simulacao_min):
+        if (servidor_ocupado and servidor_ficara_livre_no_min == i+1):
+           servidor_ocupado = False
+           atendimentos_realizados_cont += 1
+           print('\n---------------------------------------------- ')
+           print('Servidor Ficou Livre no Min: ',i+1)
+        
+        if (servidor_ocupado == False and len(fila) > 0):
+          servidor_ocupado = True
+          servidor_ficara_livre_no_min = tempo_atendimentos[fila[0]['posicao_fila_chegada']]['intervalo'] + i+1
+          fila.pop(0)
+
+        if (tempo_chegadas[posicao_lista_chegada]['minuto_tempo'] == i+1):
+          if (servidor_ocupado == False):
+            servidor_ocupado = True
+            servidor_ficara_livre_no_min = tempo_atendimentos[posicao_lista_chegada]['intervalo'] + i+1
+            print('\n---------------------------------------------- ')
+            print('Servidor Comecou atendimento no min: ', i+1)
+            print('Tempo previsto de atendimento: ', tempo_atendimentos[posicao_lista_chegada]['intervalo'])
+            print('Minuto previsto em que ficará livre: ',servidor_ficara_livre_no_min)
+            print('\n---------------------------------------------- ')
+            posicao_lista_chegada += 1
+          else:
+            fila.append({'minuto_chegada': i, 'posicao_fila_chegada': posicao_lista_chegada })
+            tempo_chegadas[posicao_lista_chegada]['pegou_fila'] = True
+            posicao_lista_chegada += 1
+
+          
 
 
 if __name__ == '__main__':
